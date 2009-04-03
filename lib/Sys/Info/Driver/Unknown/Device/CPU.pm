12084edefbd0648c8e4dcabc86e259dc34843d6a
Burak Gürsoy <burak@cpan.org>
2009-04-03 21:01:09 +0000

    Import of BURAK/Sys-Info-Driver-Unknown-0.69_10 from CPAN.
    gitpan-cpan-distribution: Sys-Info-Driver-Unknown
    gitpan-cpan-version:      0.69_10
    gitpan-cpan-path:         BURAK/Sys-Info-Driver-Unknown-0.69_10.tar.gz
    gitpan-cpan-author:       BURAK
    gitpan-cpan-maturity:     developer

--- lib/Sys/Info/Driver/Unknown/Device/CPU.pm
+++ lib/Sys/Info/Driver/Unknown/Device/CPU.pm
@@ -3,7 +3,7 @@ use strict;
 use vars qw($VERSION $UP);
 use base qw(Sys::Info::Driver::Unknown::Device::CPU::Env);
 
-$VERSION = '0.69_01';
+$VERSION = '0.69_10';
 
 BEGIN {
     local $SIG{__DIE__};
@@ -82,7 +82,7 @@ Copyright 2006-2009 Burak Gürsoy. All rights reserved.
 =head1 LICENSE
 
 This library is free software; you can redistribute it and/or modify 
-it under the same terms as Perl itself, either Perl version 5.8.8 or, 
+it under the same terms as Perl itself, either Perl version 5.10.0 or, 
 at your option, any later version of Perl 5 you may have available.
 
 =cut
--- lib/Sys/Info/Driver/Unknown/OS.pm
+++ lib/Sys/Info/Driver/Unknown/OS.pm
@@ -4,7 +4,7 @@ use vars qw( $VERSION );
 use POSIX ();
 use Sys::Info::Constants qw( :unknown );
 
-$VERSION = '0.69_08';
+$VERSION = '0.69_10';
 
 # So, we don't support $^O yet, but we can try to emulate some features
 
@@ -157,7 +157,7 @@ Copyright 2006-2009 Burak Gürsoy. All rights reserved.
 =head1 LICENSE
 
 This library is free software; you can redistribute it and/or modify 
-it under the same terms as Perl itself, either Perl version 5.8.8 or, 
+it under the same terms as Perl itself, either Perl version 5.10.0 or, 
 at your option, any later version of Perl 5 you may have available.
 
 =cut
